cc0c736578115f0d7c0559221e33deaa42a791fe
samuel de jesus <samuel1de2jesus3@gmail.com>
2023-09-25 10:12:14 -0400

    added some starter code to test c#

---
+++ CatWorx.BadgeMaker/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+Dictionary<string, int> myScoreBoard = new Dictionary<string, int>();
+myScoreBoard.Add("firstInning", 10);
+myScoreBoard.Add("secondInning", 20);
+myScoreBoard.Add("thirdInning", 30);
+myScoreBoard.Add("fourthInning", 40);
+myScoreBoard.Add("fifthInning", 50);
+
+Console.WriteLine("----------------");
+Console.WriteLine("  Scoreboard");
+Console.WriteLine("----------------");
+Console.WriteLine("Inning |  Score");
+Console.WriteLine("   1   |    {0}", myScoreBoard["firstInning"]);
+Console.WriteLine("   2   |    {0}", myScoreBoard["secondInning"]);
+Console.WriteLine("   3   |    {0}", myScoreBoard["thirdInning"]);
+Console.WriteLine("   4   |    {0}", myScoreBoard["fourthInning"]);
+Console.WriteLine("   5   |    {0}", myScoreBoard["fifthInning"]);
+    }
+  }
+}
