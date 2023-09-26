a19d5fd944a31125f4020b0aedb41806af494985
samuel de jesus <samuel1de2jesus3@gmail.com>
2023-09-26 17:41:39 -0400

    added util folder that will hold common methods and modified the main

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -35,17 +35,18 @@ while(true)
 
         static void PrintEmployees(List<Employee> employees)
         {
-            foreach (Employee employee in employees)
-            {
-
-                Console.WriteLine($"{employee.FirstName} {employee.LastName}");
-            }
+for (int i = 0; i < employees.Count; i++) 
+{
+  string template = "{0,-10}\t{1,-20}\t{2}";
+  Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+}
         }
 
         static void Main(string[] args)
         {
             List<Employee> employees = GetEmployees();
-            PrintEmployees(employees);
+            Util.PrintEmployees(employees);
+            Util.MakeCSV(employees);
         }
     }
 }
---
+++ CatWorx.BadgeMaker/Util.cs
@@ -0,0 +1,32 @@
+// Import correct packages
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+  class Util
+  {
+    // Add List parameter to method
+    public static void PrintEmployees(List<Employee> employees) 
+    {
+      for (int i = 0; i < employees.Count; i++) 
+      {
+        string template = "{0,-10}\t{1,-20}\t{2}";
+        Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+      }
+    }
+
+    //--------------------------
+
+        public static void MakeCSV(List<Employee> employees)
+    {
+        // Check to see if folder exists
+      if (!Directory.Exists("data")) 
+      {
+        // If not, create it
+        Directory.CreateDirectory("data");
+      }
+    }
+  }
+}
