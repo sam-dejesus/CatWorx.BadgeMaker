727c8c99d258decc097f955bb725161019f40226
samuel de jesus <samuel1de2jesus3@gmail.com>
2023-09-26 16:34:25 -0400

    fixed a bug and connected employee file to program also created a public class and made the model for an employee

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -3,42 +3,49 @@ using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker
 {
-class Program
+    class Program
+    {
+     static List<Employee> GetEmployees()
 {
-  static List<string> GetEmployees()
+List<Employee> employees = new List<Employee>();
+while(true) 
+{
+  // Move the initial prompt inside the loop, so it repeats for each employee
+  Console.WriteLine("Enter first name (leave empty to exit): ");
+
+  // change input to firstName
+  string firstName = Console.ReadLine() ?? "";
+  if (firstName == "") 
   {
-    List<string> employees = new List<string>();
-    while (true)
-    {
-
-      Console.WriteLine("Please enter a name: (leave empty to exit): ");
-
-      string input = Console.ReadLine() ?? "";
-
-      if (input == "")
-      {
-        break;
-      }
-      // Create a new Employee instance
-      Employee currentEmployee = new Employee(input);
-
-      employees.Add(input);
-    }
-    return employees;
+    break;
+  }
+  // add a Console.ReadLine() for each value
+  Console.Write("Enter last name: ");
+  string lastName = Console.ReadLine() ?? "";
+  Console.Write("Enter ID: ");
+  int id = Int32.Parse(Console.ReadLine() ?? "");
+  Console.Write("Enter Photo URL:");
+  string photoUrl = Console.ReadLine() ?? "";
+  Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+  employees.Add(currentEmployee);
   }
 
-  static void PrintEmployees(List<string> employees)
-  {
-    for (int i = 0; i < employees.Count; i++)
-    {
-      Console.WriteLine(employees[i]);
-    }
-  }
-
-  static void Main(string[] args)
-  {
-    List<string> employees = GetEmployees();
-    PrintEmployees(employees);
-  }
+  return employees;
 }
+
+        static void PrintEmployees(List<Employee> employees)
+        {
+            foreach (Employee employee in employees)
+            {
+
+                Console.WriteLine($"{employee.FirstName} {employee.LastName}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Employee> employees = GetEmployees();
+            PrintEmployees(employees);
+        }
+    }
 }
--- CatWorx.BadgeMaker/employee.cs
+++ CatWorx.BadgeMaker/employee.cs
@@ -1,14 +1,30 @@
 namespace CatWorx.BadgeMaker
 {
-class Employee
-{
+
+  class Employee
+  {
   public string FirstName;
   public string LastName;
   public int Id;
   public string PhotoUrl;
-  public Employee(string firstName) {
+public Employee(string firstName, string lastName, int id, string photoUrl) {
     FirstName = firstName;
+    LastName = lastName;
+    Id = id;
+    PhotoUrl = photoUrl;
+}
+    public string GetFullName() {
+        return  FirstName + " " + LastName;
+    }
+  
+    public int GetId() {
+        return Id;
+    }
+
+    public string GetPhotoUrl() {
+        return PhotoUrl;
+
+    }
   }
 }
 
-}
