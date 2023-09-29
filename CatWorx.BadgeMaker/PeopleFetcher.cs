f99d32d9545337d72b7fafb2e944ba3363fba360
samuel de jesus <samuel1de2jesus3@gmail.com>
2023-09-29 14:11:13 -0400

    added api logic to automate badge creation by using an api to import employee data

---
+++ CatWorx.BadgeMaker/PeopleFetcher.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace  CatWorx.BadgeMaker 
+{
+  class PeopleFetcher 
+  {
+   public  static List<Employee> GetEmployees()
+{
+   List<Employee> employees = new List<Employee>();
+   while(true) 
+   {
+  
+     Console.WriteLine("Enter first name (leave empty to exit): ");
+
+  
+     string firstName = Console.ReadLine() ?? "";
+     if (firstName == "") 
+     {
+    break;
+     }
+  
+     Console.Write("Enter last name: ");
+     string lastName = Console.ReadLine() ?? "";
+     Console.Write("Enter ID: ");
+     int id = Int32.Parse(Console.ReadLine() ?? "");
+     Console.Write("Enter Photo URL:");
+     string photoUrl = Console.ReadLine() ?? "";
+     Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+     employees.Add(currentEmployee);
+     }
+
+     return employees;
+   }
+
+   async public static Task<List<Employee>> GetFromApi()
+{
+  List<Employee> employees = new List<Employee>();
+  using (HttpClient client = new HttpClient())
+{
+string response = await client.GetStringAsync("https://randomuser.me/api/?results=10&nat=us&inc=name,id,picture");
+JObject json = JObject.Parse(response);
+foreach (JToken token in json.SelectToken("results")!) {
+  // Parse JSON data
+  Employee emp = new Employee
+  (
+    token.SelectToken("name.first")!.ToString(),
+    token.SelectToken("name.last")!.ToString(),
+    Int32.Parse(token.SelectToken("id.value")!.ToString().Replace("-", "")),
+    token.SelectToken("picture.large")!.ToString()
+  );
+  employees.Add(emp);
+}
+}
+  return employees;
+}
+     }
+}
--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -6,39 +6,9 @@ namespace CatWorx.BadgeMaker
 {
     class Program
     {
- static List<Employee> GetEmployees()
-{
-List<Employee> employees = new List<Employee>();
-while(true) 
-{
-  
-  Console.WriteLine("Enter first name (leave empty to exit): ");
-
-  
-  string firstName = Console.ReadLine() ?? "";
-  if (firstName == "") 
-  {
-    break;
-  }
-  
-  Console.Write("Enter last name: ");
-  string lastName = Console.ReadLine() ?? "";
-  Console.Write("Enter ID: ");
-  int id = Int32.Parse(Console.ReadLine() ?? "");
-  Console.Write("Enter Photo URL:");
-  string photoUrl = Console.ReadLine() ?? "";
-  Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
-  employees.Add(currentEmployee);
-  }
-
-  return employees;
-}
-
-
-
       async static Task Main(string[] args)
 {
-    List<Employee> employees = GetEmployees();
+    List<Employee> employees = await PeopleFetcher.GetFromApi();
     Util.PrintEmployees(employees);
     Util.MakeCSV(employees);
     await Util.MakeBadges(employees);
